2b33fb147366a9c79fc7dd22cb3c8f0551af0c15
oliveiramnando <oliveiramnando@gmail.com>
2026-07-18 00:37:24 -0400

    very basiccrud implemented

--- verybasic/modelFinal.cs
+++ verybasic/modelFinal.cs
@@ -11,6 +11,73 @@ public class Program
     public static void Main()
     {
         List<User> users = new(); 
+
+        // CREATE  
+        CreateUser(users, new User
+        {
+            Id = 1,
+            Name = "Fernando",
+            Email = "gmail"
+        });
+
+        CreateUser(users, new User
+        {
+            Id = 2,
+            Name = "John",
+            Email = "hotmail"
+        });
+
+        // READ
+        Console.WriteLine("All users:");
+        ReadAllUsers(users);
+
+        // READ one 
+        Console.WriteLine("user with id == 2");
+        User? user = readUserById(users, 2);
+
+        if (user == null)
+        {
+            Console.WriteLine("user not found");
+        }
+        else
+        {
+            Console.WriteLine($"{user.Id}: {user.Name} - {user.Email}");
+        }
+
+        // UPDATE
+        Console.WriteLine("Updating User2");
+        bool wasUpdated = updateUser(
+            users,
+            2,
+            "jane",
+            "newemail@example.com"
+        );
+
+        Console.WriteLine(
+            wasUpdated
+                ? "User updated successfully."
+                : "User not found."
+        );
+
+        // READ AFTER UPDATE
+        Console.WriteLine("\nUsers after update:");
+        ReadAllUsers(users);
+
+        // DELETE
+        Console.WriteLine("\nDeleting user 2:");
+
+        bool wasDeleted = deleteUser(users, 2);
+
+        Console.WriteLine(
+            wasDeleted
+                ? "User deleted successfully."
+                : "User not found."
+        );
+
+        // READ AFTER DELETE
+        Console.WriteLine("\nUsers after deletion:");
+        ReadAllUsers(users);
+
     }
 
     static void CreateUser(List<User> users, User newUser)
