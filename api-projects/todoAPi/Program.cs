6cf7265145bd5b75d68029cf3165a4a14bc52871
oliveiramnando <oliveiramnando@gmail.com>
2026-07-22 13:41:27 -0400

    configuration

--- api-projects/todoAPi/Program.cs
+++ api-projects/todoAPi/Program.cs
@@ -1,8 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Data;
+using TodoApi.Services;
+using TodoApi.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
+
+string connectionString =
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException(
+        "Connection string 'DefaultConnection' was not found.");
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        connectionString,
+        sqlOptions =>
+        {
+            sqlOptions.EnableRetryOnFailure();
+        }));
+
+builder.Services.AddScoped<ITodoService, TodoService>();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -16,7 +37,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// app.UseAuthorization();
 
 app.MapControllers();
 
