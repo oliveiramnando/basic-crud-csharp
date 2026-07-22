0f05742d79b4213a1dc60ccc9afc3c9a54ea1a33
oliveiramnando <oliveiramnando@gmail.com>
2026-07-22 00:20:11 -0400

    nullable for CompletedAtUtc

--- TodoApi/Models/TodoItem.cs
+++ TodoApi/Models/TodoItem.cs
@@ -5,7 +5,7 @@ public class TodoItem
     public int Id { get; set; }
     public required string Title { get; set; }
     public string? Description { get; set; }
-    public bool IsComplete { get; set; }
+    public bool IsCompleted { get; set; }
     public DateTime CreatedAtUtc { get; set; }
-    public DateTime CompletedAtUtc { get; set; }
+    public DateTime? CompletedAtUtc { get; set; }
 }
