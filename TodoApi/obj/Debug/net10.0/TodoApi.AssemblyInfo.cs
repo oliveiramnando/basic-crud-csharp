6749d1826f37538156c43171b1f19ea1f8b374b0
oliveiramnando <oliveiramnando@gmail.com>
2026-07-21 20:35:11 -0400

    junk

--- TodoApi/obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b33fb147366a9c79fc7dd22cb3c8f0551af0c15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d4d383b5f76725819c49f71df6ee93a95ad7e1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
